47334ccfdc72023a336d7a24abc71081a2380624
Dan Leonard <daniel@danleonard.us>
2019-01-30 18:56:05 -0600

    Changed git repo of dotfiles config

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,13 +1,13 @@
 # Clone dotfiles repository into $HOME/.config
-git clone --bare git@github.com:CombustibleLemon/dotfiles.git $HOME/.config
+git clone --bare git@github.com:CombustibleLemon/dotfiles.git $HOME/.dotfiles-config
 
 # Create alias 'config' for git control of dotfiles in $HOME
-alias config='/usr/bin/git --git-dir=$HOME/.config/ --work-tree=$HOME'
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles-config/ --work-tree=$HOME'
 
 # Make backup folder for already-present config files
-mkdir -p .config-backup && \
+mkdir -p .dotfiles-config-backup && \
 config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
-xargs -I{} mv {} .config-backup/{}
+xargs -I{} mv {} .dotfiles-config-backup/{}
 
 # Check out config files from $HOME/.config into $HOME 
 config checkout
