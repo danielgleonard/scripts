1d7368a0bd22faf09b63e7350e985f7f4bf5efaf
Dan Leonard <daniel@danleonard.us>
2022-02-06 20:03:43 -0600

    Create jeff user

--- jeff.sh
+++ jeff.sh
@@ -28,14 +28,14 @@ install_dialog() {
 	apt-get -qq install dialog >/dev/null 2>&1 || error 65 "Error with dialog installation"
 }
 
-upgrade() {
+upgrade_system() {
 	dialog --title "System upgrade" --backtitle "Dan's Ngingx Setup" --msgbox "Performing a full system upgrade." 7 70
 	apt-get -qq update >/dev/null 2>&1 || error $? "Error running apt update."
 	clear
 	apt-get -qq upgrade || error $? "Error running apt upgrade."
 }
 
-sshd() {
+sshd_configure() {
 	dialog --title "sshd settings" --backtitle "Dan's Nginx Setup" --msgbox "Configuring sshd to disable passwords and require public key authentication." 7 70
 	sed "s/PubkeyAuthentication no/PubkeyAuthentication yes/g" -i /etc/ssh/sshd_config || error $? "Error enabling pubkey authentication"
 	sed "s/PasswordAuthentication yes/PasswordAuthentication no/g" -i /etc/ssh/sshd_config || error $? "Error disabling password authentication"
@@ -61,6 +61,32 @@ install_progs() {
 		# Install the program with homebrew
 		apt-get -qq install $program >/dev/null 2>&1 || error 65 "$program not installed."
 	done < /tmp/setup_jeff_progs.csv
+}
+
+add_jeff() {
+	dialog --title "Adding Jeff user" --backtitle "Dan's Ngingx Setup" --msgbox "Creating a user called \"jeff\" unless one already exists." 7 70
+	clear
+	id -u jeff &>/dev/null || sudo adduser -s /usr/bin/fish jeff
+
+	if [ ! -d "~jeff" ]; then
+		mkdir "~jeff" || error $? "Error making ~jeff"
+	fi
+	if [ ! -f "~jeff/.bashrc" ]; then
+		curl -fsSL "https://raw.githubusercontent.com/danielgleonard/dotfiles/master/.bashrc" -o "~jeff/.bashrc" || error $? "Error writing ~jeff/.bashrc file."
+	fi
+	if [ ! -f "~jeff/.vimrc" ]; then
+		curl -fsSL "https://raw.githubusercontent.com/danielgleonard/dotfiles/master/.vimrc" -o "~jeff/.vimrc" || error $? "Error writing ~jeff/.vimrc file."
+	fi
+	if [ ! -d "~jeff/.ssh" ]; then
+		mkdir "~jeff.ssh" || error $? "Error making .ssh directory in ~jeff."
+	fi
+	if [ ! -f "~jeff/.ssh/authorized_keys" ]; then
+		printf "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCwGJ9J3kwMiusr7B19zgbay/cSiiq7aQ5dceG9JahC2EGtgGOhRlXZl19TgZzMsKKC46t7E8bOQfyX5taUoHewKEqF4x12hSHvddqrGST3pmrwgedX5LZJYd7aMS0aP3sXypr9YF5RCUIDucbX2QWAQBakCLilcCsXc2/p+MwI2Evb4w022mrT7zLT+93wC7s5UsIVezp9HY4sHd+mv2IyfgSPfhJFtkujK0q6s0BUnPi5CHcBXoRRcNtNGdvdRwbKpTr6IUC6aPIV5Ij4AWNCMjnKTpg1b3fYV+jaYNyGhDQyVlzR9kylu7+98YNi/RTdHxIgapnLLv9pqIvoRJOwvXSXj/jq6Q7tN/HQog/PinsG6UK99Kms5iWwVJl5H0dnBTGkXixul2U9dPQVpik3tzUgs9ZHRK5l5syarWl0ibLdwx+e7X1n/UCFbj49f5Zo4okMWfJnL0BsIB9gmjk418BXHImBMJCwuPZRWvZp0GRQOrldKUjhqzqCACtB+uk= jeffv@DESKTOP-C7J071R\n" > "~jeff/.ssh/authorized_keys" || error $? "Error writing public key to ~jeff/.ssh."
+	fi
+
+	chown -R jeff:jeff "~jeff" || error $? "Error giving jeff ownership over ~jeff"
+	chmod 755 "~jeff/.ssh" || error $? "Error setting permissions on ~jeff/.ssh"
+	chmod 644 "~jeff/authorized_keys" || error $? "Error setting permissions on ~jeff/.ssh/authorized_keys"
 }
 
 configure_nginx() {
@@ -107,9 +133,10 @@ main() {
 	roottest
 	welcomemsg
 	install_dialog
-	upgrade
-	sshd
+	upgrade_system
+	sshd_configure
 	install_progs
+	add_jeff
 	configure_nginx
 	configure_certbot
 	closing
